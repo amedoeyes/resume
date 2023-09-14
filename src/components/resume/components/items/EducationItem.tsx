c99e7e604524a7f3ce6455f05214c8c555e300a8
amedoeyes <amedoeyes@gmail.com>
2023-09-14 15:27:50 +0300

    fix styling of education item and open button console error

--- src/components/resume/components/items/EducationItem.tsx
+++ src/components/resume/components/items/EducationItem.tsx
@@ -5,7 +5,7 @@ import styles from "../../styles";
 export default function EducationItem(props: IEducationItem) {
 	return (
 		<View style={styles.educationItem}>
-			<View style={styles.educationItem}>
+			<View style={styles.educationItemHeader}>
 				<Text>{props.title}</Text>
 				<Text>
 					{props.startDate} - {props.endDate}
--- src/components/viewer/components/OpenButton.tsx
+++ src/components/viewer/components/OpenButton.tsx
@@ -1,20 +1,17 @@
 import Resume from "@/components/resume/Resume";
 import Button from "@/components/styled/Button";
 import { RootState } from "@/store";
-import { PDFDownloadLink } from "@react-pdf/renderer";
+import { PDFDownloadLink, usePDF } from "@react-pdf/renderer";
 import Link from "next/link";
 import { useSelector } from "react-redux";
 
 export default function OpenButton() {
 	const resume = useSelector((state: RootState) => state.resume);
+	const pdf = usePDF({ document: <Resume {...resume} /> });
 
 	return (
-		<PDFDownloadLink document={<Resume {...resume} />}>
-			{({ url }) => (
-				<Link href={url || ""} target="_blank">
+				<Link href={pdf[0].url || ""} target="_blank">
 					<Button>Open</Button>
 				</Link>
-			)}
-		</PDFDownloadLink>
 	);
 }

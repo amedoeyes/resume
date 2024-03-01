7a6de64aff3085574777ad63917bbb943c5db879
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2024-03-01 21:04:05 +0200

    feat: improve resizing

--- src/components/home/ResizeHandle.tsx
+++ src/components/home/ResizeHandle.tsx
@@ -1,25 +1,73 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function ResizeHandle() {
 	const [isResizing, setIsResizing] = useState(false);
+	const animateIDRef = useRef<number>(0);
 
-	const handleMouseDown = () => setIsResizing(true);
-	const handleMouseUp = () => setIsResizing(false);
+	const moveHandle = () => {
+		const resizeHandle = document.getElementById("resize-handle");
+		const leftPanel = document.getElementById("left-panel");
+
+		if (!resizeHandle || !leftPanel) return;
+
+		const editorWidth = leftPanel.getBoundingClientRect().width;
+		resizeHandle.style.left = `${editorWidth - 4}px`;
+	};
+
+	const limitPanels = () => {
+		const resizeHandle = document.getElementById("resize-handle");
+		const leftPanel = document.getElementById("left-panel");
+		const rightPanel = document.getElementById("right-panel");
+
+		if (!resizeHandle || !leftPanel || !rightPanel) return;
+
+		const editorWidth = leftPanel.getBoundingClientRect().width;
+		const viewerWidth = rightPanel.getBoundingClientRect().width;
+		const documentWidth = document.body.getBoundingClientRect().width;
+
+		const EDITOR_MIN_WIDTH = documentWidth * 0.4;
+		const VIEWER_MIN_WIDTH = documentWidth * 0.4;
+
+		if (editorWidth <= EDITOR_MIN_WIDTH) {
+			leftPanel.style.width = `${EDITOR_MIN_WIDTH}px`;
+			moveHandle();
+		}
+
+		if (viewerWidth <= VIEWER_MIN_WIDTH) {
+			leftPanel.style.width = `${documentWidth - VIEWER_MIN_WIDTH}px`;
+			moveHandle();
+		}
+	};
+
+	const handleMouseDown = () => {
+		function animate() {
+			limitPanels();
+			animateIDRef.current = requestAnimationFrame(animate);
+		}
+		if (animateIDRef.current === 0) animate();
+
+		setIsResizing(true);
+	};
+	const handleMouseUp = () => {
+		cancelAnimationFrame(animateIDRef.current);
+		animateIDRef.current = 0;
+		setIsResizing(false);
+	};
+
+	useEffect(() => {
+		window.addEventListener("resize", limitPanels);
+		return () => {
+			cancelAnimationFrame(animateIDRef.current);
+			window.removeEventListener("resize", limitPanels);
+		};
+	}, []);
 
 	useEffect(() => {
 		const resizeHandle = document.getElementById("resize-handle");
 		const leftPanel = document.getElementById("left-panel");
 		const rightPanel = document.getElementById("right-panel");
 
-		const EDITOR_MIN_WIDTH = 750;
-		const VIEWER_MIN_WIDTH = 550;
-
 		if (!resizeHandle || !leftPanel || !rightPanel) return;
-
-		const moveHandle = () => {
-			const editorWidth = leftPanel.getBoundingClientRect().width;
-			resizeHandle.style.left = `${editorWidth - 4}px`;
-		};
 
 		moveHandle();
 
@@ -27,18 +75,7 @@ export default function ResizeHandle() {
 			if (!isResizing) return;
 
 			const { clientX } = e;
-			const editorWidth = leftPanel.getBoundingClientRect().width;
-			const viewerWidth = rightPanel.getBoundingClientRect().width;
 			const documentWidth = document.body.getBoundingClientRect().width;
-
-			if (editorWidth <= EDITOR_MIN_WIDTH && clientX < editorWidth)
-				return;
-
-			if (
-				viewerWidth <= VIEWER_MIN_WIDTH &&
-				documentWidth - clientX < viewerWidth
-			)
-				return;
 
 			leftPanel.style.width = `${clientX}px`;
 			rightPanel.style.width = `${documentWidth - clientX}px`;
@@ -47,14 +84,12 @@ export default function ResizeHandle() {
 
 		document.addEventListener("mousemove", handleMouseMove);
 		document.addEventListener("mouseup", handleMouseUp);
-		window.addEventListener("resize", moveHandle);
 
 		return () => {
 			document.removeEventListener("mousemove", handleMouseMove);
 			document.removeEventListener("mouseup", handleMouseUp);
-			window.removeEventListener("resize", moveHandle);
 		};
-	}, [isResizing]);
+	});
 
 	return (
 		<span
